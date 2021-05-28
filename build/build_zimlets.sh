1741b4363a01148a7b41842f33e0f241ea3dd186
Leonardo Sampaio <leonardosampaio@mpma.mp.br>
2021-05-28 18:33:19 -0300

    build atualizado

--- build/build_zimlets.sh
+++ build/build_zimlets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ../zimlets/client
 rm br_com_sampaio_twofa_client.zip
-/usr/bin/zip br_com_sampaio_twofa_client.zip br_com_sampaio_twofa_client.js br_com_sampaio_twofa_client.xml qrcode.jsp
+/usr/bin/zip br_com_sampaio_twofa_client.zip br_com_sampaio_twofa_client.js br_com_sampaio_twofa_client.xml br_com_sampaio_twofa_client.css 2fa.png  qrcode.jsp validate.jsp
 cd ../admin
 rm br_com_sampaio_twofa_admin.zip
 /usr/bin/zip br_com_sampaio_twofa_admin.zip br_com_sampaio_twofa_admin.js br_com_sampaio_twofa_admin.properties br_com_sampaio_twofa_admin.xml invalidate.jsp
