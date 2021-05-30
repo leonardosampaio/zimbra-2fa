24b7a20ef09d1accecabeca38a8f3caa96a5a691
Leonardo Sampaio <leonardosampaio@mpma.mp.br>
2021-05-30 18:27:32 -0300

    jettyuser e zimbra user separados

--- README.md
+++ README.md
@@ -5,36 +5,29 @@ leonardors@gmail.com**
 
 ### Installation
 
-To install do:
-
-	sudo mkdir -p /opt/zimbra/2fa
-	#put install.zip inside /opt/zimbra/2fa
-	sudo chown zimbra:zimbra /opt/zimbra/2fa
-	su - zimbra
-	cd /opt/zimbra/2fa
-	unzip install.zip
-	chmod +x install.sh
-	./install.sh
-
-Server will be restarted after installation.
-
 If needed, define theses variables inside install.sh before execution:
 
-	domain=zimbra-docker.zimbra.io
 	zimbraUser=zimbra
-	zimbraPath=/opt/zimbra
-	jettyPath=$zimbraPath"/jetty-distribution-9.3.5.v20151012"
+	jettyUser=$zimbraUser
 	mysqlZimbraDb=zimbra
 	mysqlRootUser=root
-	mysqlRootPassword=`zmlocalconfig -s | grep -Po '(?<=^mysql_root_password = )\w*$'`
+	zimbraPath=`echo "$localconfig" | grep -Po '(?<=^zimbra_home = ).*$'`
+	jettyPath=$zimbraPath"/jetty"
+	installFolder=$zimbraPath"/2fa"
 
+To install do:
+
+	#put install.zip in a temp folder, e.g. /tmp/install
+	unzip install.zip
+	chmod +x install.sh
+	sudo ./install.sh
+
+Server will be restarted after installation.
 ### Testing
 
 Use Docker image
 	
 	https://github.com/jorgedlcruz/zimbra-docker
-
-### Troubleshooting
 
 Jetty logs in
 
--- install.sh
+++ install.sh
@@ -1,45 +1,57 @@
 #!/bin/bash
 
-domain=zimbra-docker.zimbra.io
-zimbraUser=zimbra
-zimbraPath=/opt/zimbra
-jettyPath=$zimbraPath"/jetty-distribution-9.3.5.v20151012"
-mysqlZimbraDb=zimbra
-mysqlRootUser=root
-mysqlRootPassword=`zmlocalconfig -s | grep -Po '(?<=^mysql_root_password = )\w*$'`
-
-if [ "$(whoami)" != "$zimbraUser" ]; then
-        echo "Invalid user, run as $zimbraUser"
+if [ "$(whoami)" != "root" ]; then
+        echo "Invalid user, run as root"
         exit
 fi
 
+#######################################################################################
+
+zimbraUser=zimbra
+jettyUser=$zimbraUser
+
+zimbraBinPath="/opt/zimbra/bin"
+localconfig=`$zimbraBinPath/zmlocalconfig -s`
+domain=`echo "$localconfig" | grep -Po '(?<=^zimbra_server_hostname = ).*$'`
+zimbraPath=`echo "$localconfig" | grep -Po '(?<=^zimbra_home = ).*$'`
+installFolder=$zimbraPath"/2fa"
+jettyPath=$zimbraPath"/jetty"
+mysqlZimbraDb=zimbra
+mysqlRootUser=root
+mysqlRootPassword=`echo "$localconfig" | grep -Po '(?<=^mysql_root_password = ).*$'`
 date=$(date '+%Y-%m-%d')
 randomPassword=`date +%s | sha256sum | base64 | head -c 32 ; echo`
 
+#######################################################################################
+
+echo 'Creating install folder'
+sudo mkdir -p $installFolder"/backup"
+sudo chown -R $zimbraUser:$zimbraUser $installFolder
+
 echo 'Creating database';
 sed -ie "s/IDENTIFIED BY '.*';/IDENTIFIED BY '$randomPassword';/" sql/create.sql
-mysql -u $mysqlRootUser -p$mysqlRootPassword $mysqlZimbraDb < sql/create.sql
+sudo -u $zimbraUser $zimbraBinPath"/mysql" -u $mysqlRootUser -p$mysqlRootPassword $mysqlZimbraDb < sql/create.sql
 
-echo 'Installing jar library';
-cp -R dist/2fa-1.0.jar $jettyPath"/common/lib/"
+echo 'Copying jar library';
+sudo -u $jettyUser cp -R dist/2fa-1.0.jar $jettyPath"/common/lib/"
 
 echo 'Installing zimlets';
-cp -R dist/br_com_sampaio_twofa_client.zip $zimbraPath"/zimlets"
-cp -R dist/br_com_sampaio_twofa_admin.zip $zimbraPath"/zimlets"
-zmzimletctl deploy $zimbraPath"/zimlets/br_com_sampaio_twofa_client.zip"
-zmzimletctl deploy $zimbraPath"/zimlets/br_com_sampaio_twofa_admin.zip"
+sudo -u $zimbraUser cp -R dist/br_com_sampaio_twofa_client.zip $zimbraPath"/zimlets"
+sudo -u $zimbraUser cp -R dist/br_com_sampaio_twofa_admin.zip $zimbraPath"/zimlets"
+sudo -u $zimbraUser $zimbraBinPath"/zmzimletctl" deploy $zimbraPath"/zimlets/br_com_sampaio_twofa_client.zip"
+sudo -u $zimbraUser $zimbraBinPath"/zmzimletctl" deploy $zimbraPath"/zimlets/br_com_sampaio_twofa_admin.zip"
 
 echo 'Modifing login page';
-cp -R $jettyPath"/webapps/zimbra/public/login.jsp" $zimbraPath"/2fa/$date-login.jsp"
-cp -R jsp/login.8.8.15.2fa.jsp $jettyPath"/webapps/zimbra/public/login.jsp"
+md5=`/usr/bin/md5sum $jettyPath/webapps/zimbra/public/login.jsp | awk '{ print $1 }'`
+sudo -u $jettyUser cp -R $jettyPath"/webapps/zimbra/public/login.jsp" $installFolder"/backup/$date-$md5-login.jsp"
+sudo -u $jettyUser cp -R jsp/login.8.8.15.2fa.jsp $jettyPath"/webapps/zimbra/public/login.jsp"
 
 echo 'Copying 2fa configuration file';
-mkdir -p $zimbraPath"/2fa/"
-cp -R config/config.properties $zimbraPath"/2fa/config.properties"
-sed -ie "s/^mysqlPassword=.*/mysqlPassword=$randomPassword/" $zimbraPath"/2fa/config.properties"
+sudo -u $zimbraUser cp -R config/config.properties $installFolder"/config.properties"
+sudo -u $zimbraUser /bin/sed -ie "s/^mysqlPassword=.*/mysqlPassword=$randomPassword/" $installFolder"/config.properties"
 
 echo 'Activating jsp files in zimlets';
-zmprov ms "$domain" zimbraZimletJspEnabled TRUE
+sudo -u $zimbraUser $zimbraBinPath"/zmprov" ms "$domain" zimbraZimletJspEnabled TRUE
 
 echo 'Restarting Zimbra';
-zmcontrol restart
+sudo -u $zimbraUser $zimbraBinPath"/zmcontrol" restart
